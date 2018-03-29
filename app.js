9114f84063d2216580a6afe49076eb3d6ac26103
masila <abelmasila@gmail.com>
2018-03-29 15:31:22 +0300

    Initial setup

---
+++ app.js
@@ -0,0 +1,3 @@
+const fs=require('fs')
+const _= require('lodash');
+const notes=require('./notes');
---
+++ notes.js
@@ -0,0 +1,4 @@
+module.exports.addNote=()=>{
+    console.log("Add Note");
+    return 'New Note';
+}
